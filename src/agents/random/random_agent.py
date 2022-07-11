64568608c2d721dfe0b1b743ebf9ebe2f20cb45e
matin <zivdar1matin@gmail.com>
2022-07-12 04:15:53 +0430

    Refactor random_agent. Init RandomAgent class

--- src/agents/random/random_agent.py
+++ src/agents/random/random_agent.py
@@ -12,35 +12,42 @@ from kafka import KafkaConsumer, KafkaProducer
 from consumer_config import agents_config as consumer_config
 from producer_config import agents_config as producer_config
 
-producer = KafkaProducer(**producer_config)
-consumer = KafkaConsumer('agents-mailbox', **consumer_config)
 
+class RandomAgent():
+    def __init__(self):
+        self.producer = KafkaProducer(**producer_config)
+        self.consumer = KafkaConsumer('agents-mailbox', **consumer_config)
 
-while(True):
-# for msg in consumer:
-    msg_img = consumer.__next__()
-    msg_json = consumer.__next__()
-    if msg_img.key.decode() != 'image':
-        msg_img = msg_json
-    env_json = json.loads(msg_json.value.decode('utf-8'))
+    def policy(self):
+        msg_img = self.consumer.__next__()
+        msg_json = self.consumer.__next__()
+        if msg_img.key.decode() != 'image':
+            msg_img = msg_json
+        env_json = json.loads(msg_json.value.decode('utf-8'))
 
-    if env_json['state']['ended']:
-        break
+        if env_json['state']['ended']:
+            self.__del__()
+            return
 
-    # convert image bytes data to numpy array of dtype uint8
-    nparr = np.frombuffer(msg_img.value, np.uint8).reshape((env_json['state']['camera_height'], env_json['state']['camera_width'], 4), order='A')
-    # convert BGRa -> RGB
-    nparr = nparr[:, :, :3][:, :, ::-1]
-    # img = Image.fromarray(nparr, 'RGB')
-    # img.save('camera.png')
-    # cv2.imshow('image', img)
-    # cv2.waitKey(0)
+        # convert image bytes data to numpy array of dtype uint8
+        nparr = np.frombuffer(msg_img.value, np.uint8).reshape((env_json['state']['camera_height'], env_json['state']['camera_width'], 4), order='A')
+        # convert BGRa -> RGB
+        nparr = nparr[:, :, :3][:, :, ::-1]
+        # img = Image.fromarray(nparr, 'RGB')
+        # img.save('camera.png')
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
 
-    movements = ['nop', 'up', 'down', 'right', 'left', 'shift + up', 'shift + down', 'shift + right', 'shift + left']
-    movement = random.choice(movements)
+        movements = ['nop', 'up', 'down', 'right', 'left', 'shift + up', 'shift + down', 'shift + right', 'shift + left']
+        movement = random.choice(movements)
 
-    producer.send('webots-mailbox', key=b'movement', value=movement.encode('utf-8'))
-    producer.flush()
+        self.producer.send('webots-mailbox', key=b'movement', value=movement.encode('utf-8'))
+        self.producer.flush()
+    
+    def __del__(self):
+        self.producer.close()
+        self.consumer.close()
 
-producer.close()
-consumer.close()
+agent = RandomAgent()
+while True:
+    agent.policy()
