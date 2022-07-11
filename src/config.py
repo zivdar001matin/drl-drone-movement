b22ccdbf763e417316d89a5f7272a86aa29067ab
matin <zivdar1matin@gmail.com>
2022-07-11 17:09:49 +0430

    Update config

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import sys
+
+sys.path.append("..") # Adds higher directory to python modules path.
+load_dotenv()
+
+# Rewarding constants
+DESIRED_ALTITUED = 1.0
+COEFFICIENT = 0.5
